c70af3ea1ff946551cff84be5de521c488eb20a7
PKG-7 <pavel.k.group@gmail.com>
2023-01-20 13:46:30 +0300

    Folders created favicon added

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <nav>
+      <div>Logo</div>
+      <div>
+        <div>About</div>
+        <div>Programs</div>
+        <div>Steps</div>
+        <div>Questions</div>
+        <div>Get in touch</div>
+      </div>
+    </nav>
+  );
+}
